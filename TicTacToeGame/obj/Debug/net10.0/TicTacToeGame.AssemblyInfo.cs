29016a0c084db08a10329318c81f1b56d91b7705
divinemphame <mphametadiwanashe@gmail.com>
2025-11-19 17:53:15 +0200

    readme update

--- TicTacToeGame/obj/Debug/net10.0/TicTacToeGame.AssemblyInfo.cs
+++ TicTacToeGame/obj/Debug/net10.0/TicTacToeGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TicTacToeGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3402a1cd713055ad97d4c39ce158061044d283c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfce0970846eecb147f47417834d34b743a24d1a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TicTacToeGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TicTacToeGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
